44769f21eedbb2102b6d619456295252d125b61e
kawax <kawaxbiz@gmail.com>
2020-10-21 21:26:33 +0900

    format GuildStoreChannel

--- src/Models/GuildStoreChannel.php
+++ src/Models/GuildStoreChannel.php
@@ -5,139 +5,190 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Models;
+
+use CharlotteDunois\Collect\Collection;
+use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\Interfaces\GuildStoreChannelInterface;
+use CharlotteDunois\Yasmin\Traits\GuildChannelTrait;
+use CharlotteDunois\Yasmin\Utils\DataHelpers;
+use CharlotteDunois\Yasmin\Utils\Snowflake;
 
 /**
  * Represents a guild's store channel.
  *
- * @property string                                                      $id                     The channel ID.
- * @property \CharlotteDunois\Yasmin\Models\Guild                        $guild                  The associated guild.
- * @property int                                                         $createdTimestamp       The timestamp of when this channel was created.
- * @property string                                                      $name                   The channel name.
- * @property bool                                                        $nsfw                   Whether the channel is marked as NSFW or not.
- * @property string|null                                                 $parentID               The ID of the parent channel, or null.
- * @property int                                                         $position               The channel position.
- * @property \CharlotteDunois\Collect\Collection                         $permissionOverwrites   A collection of PermissionOverwrite instances, mapped by their ID.
+ * @property string $id                     The channel ID.
+ * @property Guild $guild                  The associated guild.
+ * @property int $createdTimestamp       The timestamp of when this channel was created.
+ * @property string $name                   The channel name.
+ * @property bool $nsfw                   Whether the channel is marked as NSFW or not.
+ * @property string|null $parentID               The ID of the parent channel, or null.
+ * @property int $position               The channel position.
+ * @property Collection $permissionOverwrites   A collection of PermissionOverwrite instances, mapped by their ID.
  *
- * @property \DateTime                                                   $createdAt              The DateTime instance of createdTimestamp.
- * @property \CharlotteDunois\Yasmin\Models\CategoryChannel|null         $parent                 The channel's parent, or null.
+ * @property \DateTime $createdAt              The DateTime instance of createdTimestamp.
+ * @property \CharlotteDunois\Yasmin\Models\CategoryChannel|null $parent                 The channel's parent, or null.
+ * @method string getId()
+ * @method int getCreatedTimestamp()
+ * @method string getName()
+ * @method Guild getGuild()
+ * @method int getPosition()
+ * @method Collection getPermissionOverwrites()
+ * @method null getParent()
  */
-class GuildStoreChannel extends ClientBase implements \CharlotteDunois\Yasmin\Interfaces\GuildStoreChannelInterface {
-    use \CharlotteDunois\Yasmin\Traits\GuildChannelTrait;
-    
+class GuildStoreChannel extends ClientBase implements GuildStoreChannelInterface
+{
+    use GuildChannelTrait;
+
     /**
      * The associated guild.
-     * @var \CharlotteDunois\Yasmin\Models\Guild
+     *
+     * @var Guild
      */
     protected $guild;
-    
+
     /**
      * The channel ID.
+     *
      * @var string
      */
     protected $id;
-    
+
     /**
      * The ID of the parent channel, or null.
+     *
      * @var string|null
      */
     protected $parentID;
-    
+
     /**
      * The channel name.
+     *
      * @var string
      */
     protected $name;
-    
+
     /**
      * Whether the channel is marked as NSFW or not.
+     *
      * @var bool
      */
     protected $nsfw;
-    
+
     /**
      * The channel position.
+     *
      * @var int
      */
     protected $position;
-    
+
     /**
      * A collection of PermissionOverwrite instances, mapped by their ID.
-     * @var \CharlotteDunois\Collect\Collection
+     *
+     * @var Collection
      */
     protected $permissionOverwrites;
-    
+
     /**
      * The timestamp of when this channel was created.
+     *
      * @var int
      */
     protected $createdTimestamp;
-    
+
     /**
+     * @param  Client  $client
+     * @param  Guild  $guild
+     * @param  array  $channel
+     *
      * @internal
      */
-    function __construct(\CharlotteDunois\Yasmin\Client $client, \CharlotteDunois\Yasmin\Models\Guild $guild, array $channel) {
+    function __construct(
+        Client $client,
+        Guild $guild,
+        array $channel
+    ) {
         parent::__construct($client);
         $this->guild = $guild;
-        
+
         $this->id = (string) $channel['id'];
-        
-        $this->createdTimestamp = (int) \CharlotteDunois\Yasmin\Utils\Snowflake::deconstruct($this->id)->timestamp;
-        $this->permissionOverwrites = new \CharlotteDunois\Collect\Collection();
-        
+
+        $this->createdTimestamp = (int) Snowflake::deconstruct($this->id)->timestamp;
+        $this->permissionOverwrites = new Collection();
+
         $this->_patch($channel);
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
      * @throws \RuntimeException
      * @internal
      */
-    function __get($name) {
-        if(\property_exists($this, $name)) {
+    function __get($name)
+    {
+        if (\property_exists($this, $name)) {
             return $this->$name;
         }
-        
-        switch($name) {
+
+        switch ($name) {
             case 'createdAt':
-                return \CharlotteDunois\Yasmin\Utils\DataHelpers::makeDateTime($this->createdTimestamp);
-            break;
+                return DataHelpers::makeDateTime($this->createdTimestamp);
+                break;
             case 'parent':
                 return $this->guild->channels->get($this->parentID);
-            break;
+                break;
         }
-        
+
         return parent::__get($name);
     }
-    
+
     /**
      * Automatically converts to a mention.
+     *
      * @return string
      */
-    function __toString() {
+    function __toString()
+    {
         return '<#'.$this->id.'>';
     }
-    
+
     /**
+     * @param  array  $channel
+     *
      * @return void
      * @internal
      */
-    function _patch(array $channel) {
+    function _patch(array $channel)
+    {
         $this->name = (string) ($channel['name'] ?? $this->name ?? '');
         $this->nsfw = (bool) ($channel['nsfw'] ?? $this->nsfw ?? false);
-        $this->parentID = \CharlotteDunois\Yasmin\Utils\DataHelpers::typecastVariable(($channel['parent_id'] ?? $this->parentID ?? null), 'string');
+        $this->parentID = DataHelpers::typecastVariable(
+            ($channel['parent_id'] ?? $this->parentID ?? null),
+            'string'
+        );
         $this->position = (int) ($channel['position'] ?? $this->position ?? 0);
-        
-        if(isset($channel['permission_overwrites'])) {
+
+        if (isset($channel['permission_overwrites'])) {
             $this->permissionOverwrites->clear();
-            
-            foreach($channel['permission_overwrites'] as $permission) {
-                $overwrite = new \CharlotteDunois\Yasmin\Models\PermissionOverwrite($this->client, $this, $permission);
+
+            foreach ($channel['permission_overwrites'] as $permission) {
+                $overwrite = new PermissionOverwrite($this->client, $this, $permission);
                 $this->permissionOverwrites->set($overwrite->id, $overwrite);
             }
         }
+    }
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement @method string getId()
+        // TODO: Implement @method int getCreatedTimestamp()
+        // TODO: Implement @method string getName()
+        // TODO: Implement @method \CharlotteDunois\Yasmin\Models\Guild getGuild()
+        // TODO: Implement @method int getPosition()
+        // TODO: Implement @method \CharlotteDunois\Collect\Collection getPermissionOverwrites()
+        // TODO: Implement @method null getParent()
     }
 }
