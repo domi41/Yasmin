f6ae00a5fac132f73c06417ae9b00510fa2ca76b
kawax <kawaxbiz@gmail.com>
2020-10-21 21:29:02 +0900

    format MessageApplication

--- src/Models/MessageApplication.php
+++ src/Models/MessageApplication.php
@@ -5,92 +5,112 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Models;
+
+use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\HTTP\APIEndpoints;
 
 /**
  * Represents a message application.
  *
- * @property string       $id           The ID of the application.
- * @property string       $name         The name of the application.
- * @property string|null  $icon         The hash of the application icon, or null.
- * @property string|null  $coverImage   The hash of the application cover image, or null.
- * @property string       $description  The description of the application.
+ * @property string $id           The ID of the application.
+ * @property string $name         The name of the application.
+ * @property string|null $icon         The hash of the application icon, or null.
+ * @property string|null $coverImage   The hash of the application cover image, or null.
+ * @property string $description  The description of the application.
  */
-class MessageApplication extends ClientBase {
+class MessageApplication extends ClientBase
+{
     /**
      * The ID of the application.
+     *
      * @var string
      */
     protected $id;
-    
+
     /**
      * The name of the application.
+     *
      * @var string
      */
     protected $name;
-    
+
     /**
      * The hash of the application icon.
+     *
      * @var string|null
      */
     protected $icon;
-    
+
     /**
      * The hash of the application cover image.
+     *
      * @var string|null
      */
     protected $coverImage;
-    
+
     /**
      * The description of the application.
+     *
      * @var string
      */
     protected $description;
-    
+
     /**
+     * @param  Client  $client
+     * @param  array  $application
+     *
      * @internal
      */
-    function __construct(\CharlotteDunois\Yasmin\Client $client, array $application) {
+    function __construct(Client $client, array $application)
+    {
         parent::__construct($client);
-        
+
         $this->id = (string) $application['id'];
         $this->name = (string) $application['name'];
         $this->icon = $application['icon'] ?? null;
         $this->coverImage = $application['cover_image'] ?? null;
         $this->description = (string) $application['description'];
-        
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
      * @throws \RuntimeException
      * @internal
      */
-    function __get($name) {
-        if(\property_exists($this, $name)) {
+    function __get($name)
+    {
+        if (property_exists($this, $name)) {
             return $this->$name;
         }
-        
+
         return parent::__get($name);
     }
-    
+
     /**
      * Returns the URL of the cover image.
-     * @param int|null  $size  Any powers of 2 (16-2048).
+     *
+     * @param  int|null  $size  Any powers of 2 (16-2048).
+     *
      * @return string|null
      */
-    function getCoverImageURL(?int $size = null) {
-        if($size & ($size - 1)) {
+    function getCoverImageURL(?int $size = null)
+    {
+        if ($size & ($size - 1)) {
             throw new \InvalidArgumentException('Invalid size "'.$size.'", expected any powers of 2');
         }
-        
-        if($this->coverImage !== null) {
-            return \CharlotteDunois\Yasmin\HTTP\APIEndpoints::CDN['url'].\CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(\CharlotteDunois\Yasmin\HTTP\APIEndpoints::CDN['appicons'], $this->id, $this->coverImage).(!empty($size) ? '?size='.$size : '');
+
+        if ($this->coverImage !== null) {
+            return APIEndpoints::CDN['url'].APIEndpoints::format(
+                    APIEndpoints::CDN['appicons'],
+                    $this->id,
+                    $this->coverImage
+                ).(! empty($size) ? '?size='.$size : '');
         }
-        
+
         return null;
     }
 }
