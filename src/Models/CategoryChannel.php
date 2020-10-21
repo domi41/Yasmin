cc37c6c56dfdc33cb50ef909a74e171f9a1234ba
kawax <kawaxbiz@gmail.com>
2020-10-21 21:09:38 +0900

    format CategoryChannel

--- src/Models/CategoryChannel.php
+++ src/Models/CategoryChannel.php
@@ -5,118 +5,152 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Models;
+
+use CharlotteDunois\Collect\Collection;
+use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\Traits\GuildChannelTrait;
+use CharlotteDunois\Yasmin\Utils\DataHelpers;
+use CharlotteDunois\Yasmin\Utils\Snowflake;
 
 /**
  * Represents a guild's category channel.
  *
- * @property string                                               $id                     The ID of the channel.
- * @property string                                               $name                   The channel name.
- * @property \CharlotteDunois\Yasmin\Models\Guild                 $guild                  The guild this category channel belongs to.
- * @property int                                                  $createdTimestamp       The timestamp of when this channel was created.
- * @property int                                                  $position               The channel position.
- * @property \CharlotteDunois\Collect\Collection                  $permissionOverwrites   A collection of PermissionOverwrite instances.
+ * @property string $id                     The ID of the channel.
+ * @property string $name                   The channel name.
+ * @property Guild $guild                  The guild this category channel belongs to.
+ * @property int $createdTimestamp       The timestamp of when this channel was created.
+ * @property int $position               The channel position.
+ * @property Collection $permissionOverwrites   A collection of PermissionOverwrite instances.
  *
- * @property \DateTime                                            $createdAt              The DateTime instance of createdTimestamp.
+ * @property \DateTime $createdAt              The DateTime instance of createdTimestamp.
  */
-class CategoryChannel extends ClientBase implements \CharlotteDunois\Yasmin\Interfaces\CategoryChannelInterface {
-    use \CharlotteDunois\Yasmin\Traits\GuildChannelTrait;
-    
+class CategoryChannel extends ClientBase implements \CharlotteDunois\Yasmin\Interfaces\CategoryChannelInterface
+{
+    use GuildChannelTrait;
+
     /**
      * The guild this category channel belongs to.
-     * @var \CharlotteDunois\Yasmin\Models\Guild
+     *
+     * @var Guild
      */
     protected $guild;
-    
+
     /**
      * The ID of the channel.
+     *
      * @var string
      */
     protected $id;
-    
+
     /**
      * The channel name.
+     *
      * @var string
      */
     protected $name;
-    
+
     /**
      * The channel position.
+     *
      * @var int
      */
     protected $position;
-    
+
     /**
      * A collection of PermissionOverwrite instances.
-     * @var \CharlotteDunois\Collect\Collection
+     *
+     * @var Collection
      */
     protected $permissionOverwrites;
-    
+
     /**
      * The timestamp of when this channel was created.
+     *
      * @var int
      */
     protected $createdTimestamp;
-    
+
     /**
+     * @param  Client  $client
+     * @param  Guild  $guild
+     * @param  array  $channel
+     *
      * @internal
      */
-    function __construct(\CharlotteDunois\Yasmin\Client $client, \CharlotteDunois\Yasmin\Models\Guild $guild, array $channel) {
+    function __construct(
+        Client $client,
+        Guild $guild,
+        array $channel
+    ) {
         parent::__construct($client);
         $this->guild = $guild;
-        
+
         $this->id = (string) $channel['id'];
-        $this->createdTimestamp = (int) \CharlotteDunois\Yasmin\Utils\Snowflake::deconstruct($this->id)->timestamp;
-        $this->permissionOverwrites = new \CharlotteDunois\Collect\Collection();
-        
+        $this->createdTimestamp = (int) Snowflake::deconstruct($this->id)->timestamp;
+        $this->permissionOverwrites = new Collection();
+
         $this->_patch($channel);
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
      * @throws \RuntimeException
      * @internal
      */
-    function __get($name) {
-        if(\property_exists($this, $name)) {
+    function __get($name)
+    {
+        if (property_exists($this, $name)) {
             return $this->$name;
         }
-        
-        switch($name) {
+
+        switch ($name) {
             case 'createdAt':
-                return \CharlotteDunois\Yasmin\Utils\DataHelpers::makeDateTime($this->createdTimestamp);
-            break;
+                return DataHelpers::makeDateTime($this->createdTimestamp);
+                break;
         }
-        
+
         return parent::__get($name);
     }
-    
+
     /**
      * Returns all channels which are childrens of this category.
+     *
      * @return \CharlotteDunois\Yasmin\Interfaces\StorageInterface
      */
-    function getChildren() {
-        return $this->guild->channels->filter(function ($channel) {
-            return ($channel->parentID === $this->id);
-        });
+    function getChildren()
+    {
+        return $this->guild->channels->filter(
+            function ($channel) {
+                return ($channel->parentID === $this->id);
+            }
+        );
     }
-    
+
     /**
      * @return void
      * @internal
      */
-    function _patch(array $channel) {
+    function _patch(array $channel)
+    {
         $this->name = (string) ($channel['name'] ?? $this->name ?? '');
         $this->position = (int) ($channel['position'] ?? $this->position ?? 0);
-        
-        if(isset($channel['permission_overwrites'])) {
+
+        if (isset($channel['permission_overwrites'])) {
             $this->permissionOverwrites->clear();
-            
-            foreach($channel['permission_overwrites'] as $permission) {
-                $this->permissionOverwrites->set($permission['id'], new \CharlotteDunois\Yasmin\Models\PermissionOverwrite($this->client, $this, $permission));
+
+            foreach ($channel['permission_overwrites'] as $permission) {
+                $this->permissionOverwrites->set(
+                    $permission['id'],
+                    new PermissionOverwrite(
+                        $this->client,
+                        $this,
+                        $permission
+                    )
+                );
             }
         }
     }
