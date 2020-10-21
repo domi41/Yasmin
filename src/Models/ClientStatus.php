2720e825da67db3dacbfacca81c937c9d578bf59
kawax <kawaxbiz@gmail.com>
2020-10-21 21:15:00 +0900

    format ClientStatus

--- src/Models/ClientStatus.php
+++ src/Models/ClientStatus.php
@@ -5,86 +5,100 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Models;
 
 /**
  * Represents an user's client status.
  *
- * @property string|null  $desktop  The status of the user on the desktop client.
- * @property string|null  $mobile   The status of the user on the mobile client.
- * @property string|null  $web      The status of the user on the web client.
+ * @property string|null $desktop  The status of the user on the desktop client.
+ * @property string|null $mobile   The status of the user on the mobile client.
+ * @property string|null $web      The status of the user on the web client.
  */
-class ClientStatus extends Base {
+class ClientStatus extends Base
+{
     /**
      * Client status: online.
+     *
      * @var string
      * @source
      */
     const STATUS_ONLINE = 'online';
-    
+
     /**
      * Client status: do not disturb.
+     *
      * @var string
      * @source
      */
     const STATUS_DND = 'dnd';
-    
+
     /**
      * Client status: idle.
+     *
      * @var string
      * @source
      */
     const STATUS_IDLE = 'idle';
-    
+
     /**
      * Client status: offline.
+     *
      * @var string
      * @source
      */
-    CONST STATUS_OFFLINE = 'offline';
-    
+    const STATUS_OFFLINE = 'offline';
+
     /**
      * The status of the user on the desktop client.
+     *
      * @var string|null
      */
     protected $desktop;
-    
+
     /**
      * The status of the user on the mobile client.
+     *
      * @var string|null
      */
     protected $mobile;
-    
+
     /**
      * The status of the user on the web client.
+     *
      * @var string|null
      */
     protected $web;
 
     /**
      * Constructs a new instance.
-     * @param array  $clientStatus  An array containing the client status data.
+     *
+     * @param  array  $clientStatus  An array containing the client status data.
+     *
      * @internal
      */
-    function __construct(array $clientStatus) {
+    function __construct(array $clientStatus)
+    {
+        parent::__construct();
+
         $this->desktop = $clientStatus['desktop'] ?? null;
         $this->mobile = $clientStatus['mobile'] ?? null;
         $this->web = $clientStatus['web'] ?? null;
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
      * @throws \RuntimeException
      * @internal
      */
-    function __get($name) {
-        if(\property_exists($this, $name)) {
+    function __get($name)
+    {
+        if (property_exists($this, $name)) {
             return $this->$name;
         }
-        
+
         return parent::__get($name);
     }
 
@@ -92,11 +106,12 @@ class ClientStatus extends Base {
      * @return mixed
      * @internal
      */
-    function jsonSerialize() {
-        return array(
+    function jsonSerialize()
+    {
+        return [
             'desktop' => $this->desktop,
-            'mobile' => $this->mobile,
-            'web' => $this->web,
-        );
+            'mobile'  => $this->mobile,
+            'web'     => $this->web,
+        ];
     }
 }
