4cc0d0f2ff5f90b22e485d3c91ab24f6689aecde
kawax <kawaxbiz@gmail.com>
2020-10-21 20:51:27 +0900

    format ImageHelpers

--- src/Utils/ImageHelpers.php
+++ src/Utils/ImageHelpers.php
@@ -5,29 +5,36 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Utils;
 
 /**
  * Image Helper utilities.
  */
-class ImageHelpers {
+class ImageHelpers
+{
     /**
      * Returns the default extension for an image.
-     * @param string  $image  The image hash.
+     *
+     * @param  string  $image  The image hash.
+     *
      * @return string  Returns "gif" if the hash begins with "a_", otherwise "png".
      */
-    static function getImageExtension(string $image): string {
-        return (\strpos($image, 'a_') === 0 ? 'gif' : 'png');
+    public static function getImageExtension(string $image): string
+    {
+        return (strpos($image, 'a_') === 0 ? 'gif' : 'png');
     }
-    
+
     /**
      * Returns whether the input number is a power of 2.
-     * @param int|null  $size
+     *
+     * @param  int|null  $size
+     *
      * @return bool
      */
-    static function isPowerOfTwo(?int $size): bool {
-        return ($size === null || !($size & ($size - 1)));
+    public static function isPowerOfTwo(?int $size): bool
+    {
+        return ($size === null || ! ($size & ($size - 1)));
     }
 }
