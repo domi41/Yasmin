f78000eebaecf079581f7a6ab71e82089161f92b
kawax <kawaxbiz@gmail.com>
2020-10-22 14:33:17 +0900

    format MessageMentions

--- src/Models/MessageMentions.php
+++ src/Models/MessageMentions.php
@@ -5,147 +5,166 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Models;
+
+use CharlotteDunois\Collect\Collection;
+use CharlotteDunois\Yasmin\Client;
 
 /**
  * Holds message mentions.
  *
- * @property \CharlotteDunois\Yasmin\Models\Message  $message   The message these mentions belongs to.
- * @property \CharlotteDunois\Collect\Collection     $channels  The collection which holds all channel mentions, mapped by their ID.
- * @property bool                                    $everyone  Whether the message mentions @everyone or @here.
- * @property \CharlotteDunois\Collect\Collection     $members   The collection which holds all members mentions (only in guild channels), mapped by their ID. Only cached members can be put into this Collection.
- * @property \CharlotteDunois\Collect\Collection     $roles     The collection which holds all roles mentions, mapped by their ID.
- * @property \CharlotteDunois\Collect\Collection     $users     The collection which holds all users mentions, mapped by their ID.
+ * @property Message $message   The message these mentions belongs to.
+ * @property Collection $channels  The collection which holds all channel mentions, mapped by their ID.
+ * @property bool $everyone  Whether the message mentions @everyone or @here.
+ * @property Collection $members   The collection which holds all members mentions (only in guild channels), mapped by their ID. Only cached members can be put into this Collection.
+ * @property Collection $roles     The collection which holds all roles mentions, mapped by their ID.
+ * @property Collection $users     The collection which holds all users mentions, mapped by their ID.
  */
-class MessageMentions extends ClientBase {
+class MessageMentions extends ClientBase
+{
     /**
      * RegEx pattern to match channel mentions.
+     *
      * @var string
      * @source
      */
-     const PATTERN_CHANNELS = '/<#(\d+)>/';
-     
+    const PATTERN_CHANNELS = '/<#(\d+)>/';
+
     /**
      * RegEx pattern to match custom emoji mentions.
+     *
      * @var string
      * @source
      */
     const PATTERN_EMOJIS = '/<a?:(?:.*?):(\d+)>/';
-    
+
     /**
      * RegEx pattern to match role mentions.
+     *
      * @var string
      * @source
      */
     const PATTERN_ROLES = '/<@&(\d+)>/';
-    
+
     /**
      * RegEx pattern to match user mentions.
+     *
      * @var string
      * @source
      */
     const PATTERN_USERS = '/<@!?(\d+)>/';
-    
+
     /**
      * The message these mentions belongs to.
-     * @var \CharlotteDunois\Yasmin\Models\Message
+     *
+     * @var Message
      */
     protected $message;
-    
+
     /**
      * The collection which holds all channel mentions, mapped by their ID.
-     * @var \CharlotteDunois\Collect\Collection
+     *
+     * @var Collection
      */
     protected $channels;
-    
+
     /**
      * Whether the message mentions @everyone or @here.
+     *
      * @var bool
      */
     protected $everyone;
-    
+
     /**
      * The collection which holds all members mentions (only in guild channels), mapped by their ID. Only cached members can be put into this Collection.
-     * @var \CharlotteDunois\Collect\Collection
+     *
+     * @var Collection
      */
     protected $members;
-    
+
     /**
      * The collection which holds all roles mentions, mapped by their ID.
-     * @var \CharlotteDunois\Collect\Collection
+     *
+     * @var Collection
      */
     protected $roles;
-    
+
     /**
      * The collection which holds all users mentions, mapped by their ID.
-     * @var \CharlotteDunois\Collect\Collection
+     *
+     * @var Collection
      */
     protected $users;
-    
+
     /**
      * @internal
      */
-    function __construct(\CharlotteDunois\Yasmin\Client $client, \CharlotteDunois\Yasmin\Models\Message $message, array $msg) {
+    function __construct(
+        Client $client,
+        Message $message,
+        array $msg
+    ) {
         parent::__construct($client);
         $this->message = $message;
-        
-        $this->channels = new \CharlotteDunois\Collect\Collection();
-        $this->members = new \CharlotteDunois\Collect\Collection();
-        $this->roles = new \CharlotteDunois\Collect\Collection();
-        $this->users = new \CharlotteDunois\Collect\Collection();
-        
-        $this->everyone = !empty($msg['mention_everyone']);
-        
+
+        $this->channels = new Collection();
+        $this->members = new Collection();
+        $this->roles = new Collection();
+        $this->users = new Collection();
+
+        $this->everyone = ! empty($msg['mention_everyone']);
+
         \preg_match_all(self::PATTERN_CHANNELS, $message->content, $matches);
-        if(!empty($matches[1])) {
-            foreach($matches[1] as $match) {
+        if (! empty($matches[1])) {
+            foreach ($matches[1] as $match) {
                 $channel = $this->client->channels->get($match);
-                if($channel) {
+                if ($channel) {
                     $this->channels->set($channel->getId(), $channel);
                 }
             }
         }
-        
-        if(!empty($msg['mentions'])) {
-            foreach($msg['mentions'] as $mention) {
+
+        if (! empty($msg['mentions'])) {
+            foreach ($msg['mentions'] as $mention) {
                 $user = $this->client->users->patch($mention);
-                if($user) {
+                if ($user) {
                     $member = null;
-                    
+
                     $this->users->set($user->id, $user);
-                    if($message->guild) {
+                    if ($message->guild) {
                         $member = $message->guild->members->get($user->id);
-                        if($member) {
+                        if ($member) {
                             $this->members->set($member->id, $member);
                         }
                     }
                 }
             }
         }
-        
-        if($message->channel instanceof \CharlotteDunois\Yasmin\Models\TextChannel && !empty($msg['mention_roles'])) {
-            foreach($msg['mention_roles'] as $id) {
+
+        if ($message->channel instanceof TextChannel && ! empty($msg['mention_roles'])) {
+            foreach ($msg['mention_roles'] as $id) {
                 $role = $message->channel->guild->roles->get($id);
-                if($role) {
+                if ($role) {
                     $this->roles->set($role->id, $role);
                 }
             }
         }
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
      * @throws \RuntimeException
      * @internal
      */
-    function __get($name) {
-        if(\property_exists($this, $name)) {
+    function __get($name)
+    {
+        if (\property_exists($this, $name)) {
             return $this->$name;
         }
-        
+
         return parent::__get($name);
     }
 }
