6d553872e9e9a8197c4c829510cd534111b7e852
kawax <kawaxbiz@gmail.com>
2020-10-22 14:50:07 +0900

    format Collector

--- src/Utils/Collector.php
+++ src/Utils/Collector.php
@@ -5,69 +5,73 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Utils;
+
+use CharlotteDunois\Collect\Collection;
+use React\Promise\Promise;
 
 /**
  * A collector is an util to collect elements from events.
  */
-class Collector {
+class Collector
+{
     /**
      * @var \CharlotteDunois\Events\EventEmitterInterface
      */
     protected $emitter;
-    
+
     /**
      * @var string
      */
     protected $event;
-    
+
     /**
      * @var callable
      */
     protected $handler;
-    
+
     /**
      * @var callable|null
      */
     protected $filter;
-    
+
     /**
      * @var array
      */
     protected $options;
-    
+
     /**
      * @var \Closure
      */
     protected $listener;
-    
+
     /**
      * @var \Closure
      */
     protected $resolve;
-    
+
     /**
      * @var \Closure
      */
     protected $reject;
-    
+
     /**
-     * @var \CharlotteDunois\Collect\Collection
+     * @var Collection
      */
     protected $bucket;
-    
+
     /**
      * @var \React\EventLoop\TimerInterface|\React\EventLoop\Timer\TimerInterface
      */
     protected $timer;
-    
+
     /**
      * @var \React\EventLoop\LoopInterface
      */
     protected static $loop;
-    
+
     /**
      * The handler gets invoked as `$handler(...$item)`. The optional filter get applied to look for a specific event (invoked as `$filter(...$item)`).
      *
@@ -80,93 +84,108 @@ class Collector {
      * )
      * ```
      *
-     * @param \CharlotteDunois\Events\EventEmitterInterface  $emitter
-     * @param string                                         $event
-     * @param callable                                       $handler  How the collect item should be handled. Must return an array of `[ $key, $value ]`.
-     * @param callable|null                                  $filter
-     * @param array                                          $options
+     * @param  \CharlotteDunois\Events\EventEmitterInterface  $emitter
+     * @param  string  $event
+     * @param  callable  $handler  How the collect item should be handled. Must return an array of `[ $key, $value ]`.
+     * @param  callable|null  $filter
+     * @param  array  $options
      */
-    function __construct($emitter, string $event, callable $handler, ?callable $filter = null, array $options = array()) {
+    function __construct($emitter, string $event, callable $handler, ?callable $filter = null, array $options = [])
+    {
         $this->emitter = $emitter;
         $this->event = $event;
         $this->handler = $handler;
         $this->filter = $filter;
         $this->options = $options;
-        $this->bucket = new \CharlotteDunois\Collect\Collection();
+        $this->bucket = new Collection();
     }
-    
+
     /**
      * Sets the Event Loop.
-     * @param \React\EventLoop\LoopInterface  $loop
+     *
+     * @param  \React\EventLoop\LoopInterface  $loop
+     *
      * @return void
      * @internal
      */
-    static function setLoop(\React\EventLoop\LoopInterface $loop) {
+    static function setLoop(\React\EventLoop\LoopInterface $loop)
+    {
         self::$loop = $loop;
     }
-    
+
     /**
      * Starts collecting. Resolves with a collection.
+     *
      * @return \React\Promise\ExtendedPromiseInterface   This promise is cancellable.
      * @throws \RangeException          The exception the promise gets rejected with, if collecting times out.
      * @throws \OutOfBoundsException    The exception the promise gets rejected with, if the promise gets cancelled.
      */
-    function collect() {
-        return (new \React\Promise\Promise(function (callable $resolve, callable $reject) {
-            $this->resolve = $resolve;
-            $this->reject = $reject;
-            
-            $filter = $this->filter;
-            $handler = $this->handler;
-            
-            $this->listener = function (...$item) use (&$filter, &$handler) {
-                if($filter === null || $filter(...$item)) {
-                    list($key, $value) = $handler(...$item);
-                    $this->bucket->set($key, $value);
-                    
-                    if(($this->options['max'] ?? \INF) <= $this->bucket->count()) {
-                        $this->stop();
+    function collect()
+    {
+        return (new Promise(
+            function (callable $resolve, callable $reject) {
+                $this->resolve = $resolve;
+                $this->reject = $reject;
+
+                $filter = $this->filter;
+                $handler = $this->handler;
+
+                $this->listener = function (...$item) use (&$filter, &$handler) {
+                    if ($filter === null || $filter(...$item)) {
+                        [$key, $value] = $handler(...$item);
+                        $this->bucket->set($key, $value);
+
+                        if (($this->options['max'] ?? \INF) <= $this->bucket->count()) {
+                            $this->stop();
+                        }
                     }
+                };
+
+                if (($this->options['time'] ?? 30) > 0) {
+                    $this->timer = self::$loop->addTimer(
+                        ($this->options['time'] ?? 30),
+                        function () {
+                            $this->stop();
+                        }
+                    );
                 }
-            };
-            
-            if(($this->options['time'] ?? 30) > 0) {
-                $this->timer = self::$loop->addTimer(($this->options['time'] ?? 30), function () {
-                    $this->stop();
-                });
-            }
-            
-            $this->emitter->on($this->event, $this->listener);
-        }, function (callable $resolve, callable $reject) {
-            if($this->timer) {
+
+                $this->emitter->on($this->event, $this->listener);
+            }, function (callable $resolve, callable $reject) {
+            if ($this->timer) {
                 self::$loop->cancelTimer($this->timer);
                 $this->timer = null;
             }
-            
+
             $this->emitter->removeListener($this->event, $this->listener);
             $reject(new \OutOfBoundsException('Operation cancelled'));
-        }));
+        }
+        ));
     }
-    
+
     /**
      * This will stop the collector.
+     *
      * @return void
      */
-    function stop() {
-        if($this->timer) {
+    function stop()
+    {
+        if ($this->timer) {
             self::$loop->cancelTimer($this->timer);
             $this->timer = null;
         }
-        
+
         $this->emitter->removeListener($this->event, $this->listener);
-        
-        $errors = (array) ($this->options['errors'] ?? array());
-        if(\in_array('max', $errors, true) && ($this->options['max'] ?? 0) > 0 && $this->bucket->count() < $this->options['max']) {
+
+        $errors = (array) ($this->options['errors'] ?? []);
+        if (\in_array('max', $errors, true) && ($this->options['max'] ?? 0) > 0 && $this->bucket->count(
+            ) < $this->options['max']) {
             $reject = $this->reject;
             $reject(new \RangeException('Collecting timed out (max not reached in time)'));
+
             return;
         }
-        
+
         $resolve = $this->resolve;
         $resolve($this->bucket);
     }
