c90255b822d2e1f9184e9175512dbaa6e5d30936
kawax <kawaxbiz@gmail.com>
2020-10-22 14:54:47 +0900

    format ChannelCreate

--- src/WebSocket/Events/ChannelCreate.php
+++ src/WebSocket/Events/ChannelCreate.php
@@ -5,44 +5,62 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\WebSocket\Events;
 
+use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\Interfaces\GuildChannelInterface;
+use CharlotteDunois\Yasmin\Interfaces\WSEventInterface;
+use CharlotteDunois\Yasmin\Models\GuildMember;
+use CharlotteDunois\Yasmin\WebSocket\WSConnection;
+use CharlotteDunois\Yasmin\WebSocket\WSManager;
+
 /**
  * WS Event
+ *
  * @see https://discordapp.com/developers/docs/topics/gateway#channel-create
  * @internal
  */
-class ChannelCreate implements \CharlotteDunois\Yasmin\Interfaces\WSEventInterface {
+class ChannelCreate implements WSEventInterface
+{
     /**
      * The client.
-     * @var \CharlotteDunois\Yasmin\Client
+     *
+     * @var Client
      */
     protected $client;
-    
-    function __construct(\CharlotteDunois\Yasmin\Client $client, \CharlotteDunois\Yasmin\WebSocket\WSManager $wsmanager) {
+
+    function __construct(Client $client, WSManager $wsmanager)
+    {
         $this->client = $client;
     }
-    
-    function handle(\CharlotteDunois\Yasmin\WebSocket\WSConnection $ws, $data): void {
+
+    function handle(WSConnection $ws, $data): void
+    {
         $channel = $this->client->channels->factory($data);
-        
-        $prom = array();
-        if($channel instanceof \CharlotteDunois\Yasmin\Interfaces\GuildChannelInterface) {
-            foreach($channel->getPermissionOverwrites() as $overwrite) {
-                if($overwrite->type === 'member' && $overwrite->target === null) {
-                    $prom[] = $channel->getGuild()->fetchMember($overwrite->id)->then(function (\CharlotteDunois\Yasmin\Models\GuildMember $member) use ($overwrite) {
-                        $overwrite->_patch(array('target' => $member));
-                    }, function () {
-                        // Do nothing
-                    });
+
+        $prom = [];
+        if ($channel instanceof GuildChannelInterface) {
+            foreach ($channel->getPermissionOverwrites() as $overwrite) {
+                if ($overwrite->type === 'member' && $overwrite->target === null) {
+                    $prom[] = $channel->getGuild()->fetchMember($overwrite->id)->then(
+                        function (GuildMember $member) use ($overwrite) {
+                            $overwrite->_patch(['target' => $member]);
+                        },
+                        function () {
+                            // Do nothing
+                        }
+                    );
                 }
             }
         }
-        
-        \React\Promise\all($prom)->done(function () use ($channel) {
-            $this->client->queuedEmit('channelCreate', $channel);
-        }, array($this->client, 'handlePromiseRejection'));
+
+        \React\Promise\all($prom)->done(
+            function () use ($channel) {
+                $this->client->queuedEmit('channelCreate', $channel);
+            },
+            [$this->client, 'handlePromiseRejection']
+        );
     }
 }
