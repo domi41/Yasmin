8ae17d3341a98e35bf343a642d57efea01f4bd4a
kawax <kawaxbiz@gmail.com>
2020-10-22 14:42:15 +0900

    format Shard

--- src/Models/Shard.php
+++ src/Models/Shard.php
@@ -5,68 +5,83 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\Models;
+
+use CharlotteDunois\Yasmin\Client;
+use CharlotteDunois\Yasmin\WebSocket\WSConnection;
+use Serializable;
 
 /**
  * Represents a shard.
  *
- * @property int  $id  The shard ID.
+ * @property int $id  The shard ID.
  */
-class Shard extends ClientBase implements \Serializable {
+class Shard extends ClientBase implements Serializable
+{
     /**
      * The shard ID.
+     *
      * @var int
      */
     protected $id;
-    
+
     /**
      * The websocket connection of this shard.
-     * @var \CharlotteDunois\Yasmin\WebSocket\WSConnection
+     *
+     * @var WSConnection
      */
     protected $ws;
-    
+
     /**
+     * @param  Client  $client
+     * @param  int  $shardID
+     * @param  WSConnection  $connection
+     *
      * @internal
      */
-    function __construct(\CharlotteDunois\Yasmin\Client $client, int $shardID, \CharlotteDunois\Yasmin\WebSocket\WSConnection $connection) {
+    function __construct(Client $client, int $shardID, WSConnection $connection)
+    {
         parent::__construct($client);
-        
+
         $this->id = $shardID;
         $this->ws = $connection;
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
      * @throws \RuntimeException
      * @internal
      */
-    function __get($name) {
-        if(\property_exists($this, $name)) {
+    function __get($name)
+    {
+        if (\property_exists($this, $name)) {
             return $this->$name;
         }
-        
+
         return parent::__get($name);
     }
-    
+
     /**
      * @return string
      * @internal
      */
-    function serialize() {
+    function serialize()
+    {
         $vars = \get_object_vars($this);
         unset($vars['client'], $vars['ws']);
-        
+
         return \serialize($vars);
     }
-    
+
     /**
      * @return string
      * @internal
      */
-    function __toString() {
+    function __toString()
+    {
         return ((string) $this->id);
     }
 }
