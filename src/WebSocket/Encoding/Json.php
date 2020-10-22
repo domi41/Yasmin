051c5d2618009ff9679f94180436e0f390c5d0bf
kawax <kawaxbiz@gmail.com>
2020-10-22 14:54:06 +0900

    format Json

--- src/WebSocket/Encoding/Json.php
+++ src/WebSocket/Encoding/Json.php
@@ -5,73 +5,97 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\WebSocket\Encoding;
 
+use CharlotteDunois\Yasmin\Interfaces\WSEncodingInterface;
+use CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException;
+use Ratchet\RFC6455\Messaging\Frame;
+use Ratchet\RFC6455\Messaging\Message;
+
 /**
  * Handles WS encoding.
+ *
  * @internal
  */
-class Json implements \CharlotteDunois\Yasmin\Interfaces\WSEncodingInterface {
+class Json implements WSEncodingInterface
+{
     /**
      * Returns encoding name (for gateway query string).
+     *
      * @return string
      */
-    function getName(): string {
+    function getName(): string
+    {
         return 'json';
     }
-    
+
     /**
      * Checks if the system supports it.
+     *
      * @return void
      * @throws \RuntimeException
      */
-    static function supported(): void {
+    static function supported(): void
+    {
         // Nothing to check
     }
-    
+
     /**
      * Decodes data.
-     * @param string  $data
+     *
+     * @param  string  $data
+     *
      * @return mixed
-     * @throws \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException
+     * @throws DiscordGatewayException
      */
-    function decode(string $data) {
+    function decode(string $data)
+    {
         $msg = \json_decode($data, true);
-        if($msg === null && \json_last_error() !== \JSON_ERROR_NONE) {
-            throw new \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException('The JSON decoder was unable to decode the data. Error: '.\json_last_error_msg());
+        if ($msg === null && \json_last_error() !== \JSON_ERROR_NONE) {
+            throw new DiscordGatewayException(
+                'The JSON decoder was unable to decode the data. Error: '.\json_last_error_msg()
+            );
         }
-        
+
         return $msg;
     }
-    
+
     /**
      * Encodes data.
-     * @param mixed  $data
+     *
+     * @param  mixed  $data
+     *
      * @return string
-     * @throws \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException
+     * @throws DiscordGatewayException
      */
-    function encode($data): string {
+    function encode($data): string
+    {
         $msg = \json_encode($data);
-        if($msg === false && \json_last_error() !== \JSON_ERROR_NONE) {
-            throw new \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException('The JSON encoder was unable to encode the data. Error: '.\json_last_error_msg());
+        if ($msg === false && \json_last_error() !== \JSON_ERROR_NONE) {
+            throw new DiscordGatewayException(
+                'The JSON encoder was unable to encode the data. Error: '.\json_last_error_msg()
+            );
         }
-        
+
         return $msg;
     }
-    
+
     /**
      * Prepares the data to be sent.
-     * @param string  $data
-     * @return \Ratchet\RFC6455\Messaging\Message
+     *
+     * @param  string  $data
+     *
+     * @return Message
      */
-    function prepareMessage(string $data): \Ratchet\RFC6455\Messaging\Message {
-        $frame = new \Ratchet\RFC6455\Messaging\Frame($data, true, \Ratchet\RFC6455\Messaging\Frame::OP_TEXT);
-        
-        $msg = new \Ratchet\RFC6455\Messaging\Message();
+    function prepareMessage(string $data): Message
+    {
+        $frame = new Frame($data, true, Frame::OP_TEXT);
+
+        $msg = new Message();
         $msg->addFrame($frame);
-        
+
         return $msg;
     }
 }
