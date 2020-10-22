a4d50858388d2c7c5fe81f278c4b882c9725ab5c
kawax <kawaxbiz@gmail.com>
2020-10-22 14:53:19 +0900

    format ZlibStream

--- src/WebSocket/Compression/ZlibStream.php
+++ src/WebSocket/Compression/ZlibStream.php
@@ -5,83 +5,101 @@
  *
  * Website: https://charuru.moe
  * License: https://github.com/CharlotteDunois/Yasmin/blob/master/LICENSE
-*/
+ */
 
 namespace CharlotteDunois\Yasmin\WebSocket\Compression;
 
+use CharlotteDunois\Yasmin\Interfaces\WSCompressionInterface;
+use CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException;
+
 /**
  * Handles WS compression.
+ *
  * @internal
  */
-class ZlibStream implements \CharlotteDunois\Yasmin\Interfaces\WSCompressionInterface {
+class ZlibStream implements WSCompressionInterface
+{
     /**
      * @var resource
      */
     protected $context;
-    
+
     /**
      * Checks if the system supports it.
+     *
      * @return void
      * @throws \Exception
      */
-    static function supported(): void {
-        if(!\function_exists('\inflate_init')) {
+    static function supported(): void
+    {
+        if (! \function_exists('\inflate_init')) {
             throw new \RuntimeException('Zlib is not supported by this PHP installation');
         }
     }
-    
+
     /**
      * Returns compression name (for gateway query string).
+     *
      * @return string
      */
-    static function getName(): string {
+    static function getName(): string
+    {
         return 'zlib-stream';
     }
-    
+
     /**
      * Returns a boolean for the OP code 2 IDENTIFY packet 'compress' parameter. The parameter is for payload compression.
+     *
      * @return bool
      */
-    static function isPayloadCompression(): bool {
+    static function isPayloadCompression(): bool
+    {
         return false;
     }
-    
+
     /**
      * Initializes the context.
+     *
      * @return void
      * @throws \RuntimeException
      */
-    function init(): void {
+    function init(): void
+    {
         $this->context = \inflate_init(\ZLIB_ENCODING_DEFLATE);
-        if(!$this->context) {
+        if (! $this->context) {
             throw new \RuntimeException('Unable to initialize Zlib Inflate');
         }
     }
-    
+
     /**
      * Destroys the context.
+     *
      * @return void
      */
-    function destroy(): void {
+    function destroy(): void
+    {
         $this->context = null;
     }
-    
+
     /**
      * Decompresses data.
-     * @param string  $data
+     *
+     * @param  string  $data
+     *
      * @return string
-     * @throws \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException
+     * @throws DiscordGatewayException
      */
-    function decompress(string $data): string {
-        if(!$this->context) {
-            throw new \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException('No inflate context initialized');
+    function decompress(string $data): string
+    {
+        if (! $this->context) {
+            throw new DiscordGatewayException('No inflate context initialized');
         }
-        
+
         $uncompressed = \inflate_add($this->context, $data);
-        if($uncompressed === false) {
-            throw new \CharlotteDunois\Yasmin\WebSocket\DiscordGatewayException('The inflate context was unable to decompress the data');
+        if ($uncompressed === false) {
+            throw new DiscordGatewayException('The inflate context was unable to decompress the data');
         }
-        
+
         return $uncompressed;
     }
 }
