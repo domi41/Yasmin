83a4956394e626cb265390ccd1616e925c55e381
kawax <kawaxbiz@gmail.com>
2020-10-31 17:08:05 +0900

    Update Intents

--- examples/intents_test.php
+++ examples/intents_test.php
@@ -8,5 +8,6 @@ var_dump(Intents::all());
 var_dump(Intents::default());
 var_dump(decbin(Intents::bit(Intents::default())));
 var_dump(Intents::only(['GUILD_MESSAGES']));
-var_dump(decbin(Intents::bit(Intents::only(['GUILD_MESSAGES']))));
+var_dump(decbin(Intents::bit(Intents::only(['DIRECT_MESSAGE_TYPING']))));
 var_dump(Intents::except(['GUILD_PRESENCES']));
+var_dump(decbin(Intents::bit(Intents::all())));
