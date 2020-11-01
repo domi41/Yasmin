540d171a6dc75b362143f872c4f069f7efc47344
kawax <kawaxbiz@gmail.com>
2020-11-01 12:04:19 +0900

    Update MessageCreate

--- src/WebSocket/Events/MessageCreate.php
+++ src/WebSocket/Events/MessageCreate.php
@@ -34,62 +34,75 @@ class MessageCreate implements WSEventInterface
      */
     protected $client;
 
+    /**
+     * @param  Client  $client
+     * @param  WSManager  $wsmanager
+     */
     public function __construct(Client $client, WSManager $wsmanager)
     {
         $this->client = $client;
     }
 
+    /**
+     * @param  WSConnection  $ws
+     * @param $data
+     */
     public function handle(WSConnection $ws, $data): void
     {
         $channel = $this->client->channels->get($data['channel_id']);
-        if ($channel instanceof TextChannelInterface) {
-            $user = $this->client->users->patch($data['author']);
 
-            if (! empty($data['member']) && $channel instanceof TextChannel && ! $channel->getGuild()->members->has(
-                    $user->id
-                )) {
-                $member = $data['member'];
-                $member['user'] = ['id' => $user->id];
-                $channel->getGuild()->_addMember($member, true);
-            }
-
-            $message = $channel->_createMessage($data);
-
-            if ($message->guild && $message->mentions->users->count() > 0 && $message->mentions->users->count(
-                ) > $message->mentions->members->count()) {
-                $promise = [];
-
-                foreach ($message->mentions->users as $user) {
-                    $promise[] = $message->guild->fetchMember($user->id)->then(
-                        function (GuildMember $member) use ($message) {
-                            $message->mentions->members->set($member->id, $member);
-                        },
-                        function () {
-                            // Ignore failure
-                        }
-                    );
-                }
-
-                $prm = all($promise);
-            } else {
-                $prm = resolve();
-            }
-
-            $prm->done(
-                function () use ($message) {
-                    if ($message->guild && ! ($message->member instanceof GuildMember) && ! $message->author->webhook) {
-                        return $message->guild->fetchMember($message->author->id)->then(
-                            null,
-                            function () {
-                                // Ignore failure
-                            }
-                        );
-                    }
-
-                    $this->client->queuedEmit('message', $message);
-                },
-                [$this->client, 'handlePromiseRejection']
-            );
+        if (! $channel instanceof TextChannelInterface) {
+            return;
         }
+
+        $user = $this->client->users->patch($data['author']);
+
+        if (! empty($data['member'])
+            && $channel instanceof TextChannel
+            && ! $channel->getGuild()->members->has($user->id)
+        ) {
+            $member = $data['member'];
+            $member['user'] = ['id' => $user->id];
+            $channel->getGuild()->_addMember($member, true);
+        }
+
+        $message = $channel->_createMessage($data);
+
+        if ($message->guild
+            && $message->mentions->users->count() > 0
+            && $message->mentions->users->count() > $message->mentions->members->count()
+        ) {
+            $promise = [];
+
+            foreach ($message->mentions->users as $user) {
+                $promise[] = $message->guild->fetchMember($user->id)->then(
+                    function (GuildMember $member) use ($message) {
+                        $message->mentions->members->set($member->id, $member);
+                    },
+                    function () {
+                        // Ignore failure
+                    }
+                );
+            }
+
+            $prm = all($promise);
+        } else {
+            $prm = resolve();
+        }
+
+        $prm->done(function () use ($message) {
+            if ($message->guild && ! ($message->member instanceof GuildMember) && ! $message->author->webhook) {
+                return $message->guild->fetchMember($message->author->id)->then(
+                    null,
+                    function () {
+                        // Ignore failure
+                    }
+                );
+            }
+
+            $this->client->queuedEmit('message', $message);
+        },
+            [$this->client, 'handlePromiseRejection']
+        );
     }
 }
